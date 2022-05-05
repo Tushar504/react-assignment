cb973ffe3bff616d6af92653c3cf9a77b4ae2128
Tushar Ahire <tusharmadhavahire98@gmail.com>
2022-05-05 16:11:38 +0530

    final

--- router-project-tushar-ahire/src/componants/home.jsx
+++ router-project-tushar-ahire/src/componants/home.jsx
@@ -1,3 +1,22 @@
+import { useEffect, useState } from "react"
 export const Home=()=>{
-    return <div>Home</div>
+    const [data,setData]=useState(["https://rukminim1.flixcart.com/flap/844/140/image/4313fd77ab0af5ba.jpg?q=50","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2KI2_rVsk3i3yjTnd2cHzo7x-vkqbZNUcNw&usqp=CAU","https://image.shutterstock.com/image-vector/brush-sale-banner-vector-260nw-1090866878.jpg"])
+    const [Img,setImg]=useState(data[0])
+   let i=1;
+useEffect(()=>{
+    setInterval(()=>{
+        setImg(data[i])
+        if(i==2){
+            i=0
+        }
+        else{
+            i++
+        }
+     },5000)
+},[])
+   
+             
+    return <div>
+        <img src={Img}/>
+    </div>
 }
